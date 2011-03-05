0637412057ed79cb1f1c891ec613842624fe81ed
codestation <cesarg9@gmail.com>
2011-03-05 18:24:22 +0000

    Added missing header

--- kernel/sceio.h
+++ kernel/sceio.h
@@ -1,7 +1,7 @@
 /*
- *  MHP3patch user module
+ *  MHP3patch kernel module
  *
- *  Copyright (C) 2010  Codestation
+ *  Copyright (C) 2011  Codestation
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
